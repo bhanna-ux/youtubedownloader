b8867d274a3737d27e70a42d697d9364860f3047
bhanna-ux <114748994+bhanna-ux@users.noreply.github.com>
2024-05-27 23:15:54 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -10,15 +10,16 @@ st.title(":blue[YouTube Live Downloader]")
 
 
 url = st.text_input(':blue[Add Live youtube URL here:]')
-def stream_to_url(url, quality='best'):
-    # The "audio_only" quality may be invalid for some streams (check).
-    session = Streamlink()
-    streams = session.streams(url)
-    return streams[quality].to_url()
+
 
 
 if url :
     play_file=st.video(url)
+    def stream_to_url(url, quality='best'):
+    # The "audio_only" quality may be invalid for some streams (check).
+    session = Streamlink()
+    streams = session.streams(url)
+    return streams[quality].to_url()
     
     stream_url = stream_to_url(url)
     
